4f7df732613733dd0c6fc413ea9e3932d8e6e865
이승연 <leelaf0002@gmail.com>
2025-09-02 23:19:08 +0900

    save load clear 구현

--- Assets/TutorialInfo/Scripts/JsonTest2.cs
+++ Assets/TutorialInfo/Scripts/JsonTest2.cs
@@ -17,9 +17,9 @@ public class JsonTest2 : MonoBehaviour
     public static string SaveFilePath => Path.Combine(Application.persistentDataPath, SaveFileName);
 
     public GameObject prefab;
-    public List<GameObject> prefabs;
+    public List<GameObject> prefabs = new List<GameObject>();
     public List<ObjectData> objectDataList = new List<ObjectData>();
-
+    private int spawnCount = 5;
 
     public void Start()
     {
@@ -36,7 +36,7 @@ public class JsonTest2 : MonoBehaviour
            // objdata.rotation = obj.transform.rotation;
             objectDataList.Add(objdata);
         }
-
+        Debug.Log($"Saving to: {SaveFilePath}");
         var jsonObjectData = JsonConvert.SerializeObject(objectDataList, Formatting.Indented, new Vector3Converter());
         File.WriteAllText(SaveFilePath, jsonObjectData);
     }
@@ -51,22 +51,32 @@ public class JsonTest2 : MonoBehaviour
 
         var json = File.ReadAllText(SaveFilePath);
         var transform = JsonConvert.DeserializeObject<List<ObjectData>>(json, new Vector3Converter());
-        for(int i = 0; i < prefabs.Count; i++)
+        for(int i = 0; i < transform.Count; i++)
         {
-            Instantiate(prefabs[i], transform[i].position, Quaternion.identity).transform.localScale = transform[i].scale;
+            Instantiate(prefab, transform[i].position, Quaternion.identity).transform.localScale = transform[i].scale;
         }
     }
 
     public void RemoveButton()
     {
-        var cube = GameObject.FindGameObjectWithTag("Player");
-        Destroy(cube);
+        //if (File.Exists(SaveFilePath))
+        //{
+        //    File.Delete(SaveFilePath);
+        //    Debug.Log("JSON 파일 삭제 완료: " + SaveFilePath);
+        //}
+        var findGo = GameObject.FindGameObjectsWithTag("Player");
+        for(int i = 0; i < findGo.Length; i++)
+        {
+            Destroy(findGo[i]);
+            prefabs.Remove(findGo[i]);
+        }
+        objectDataList.Clear();
+
     }
 
     public void RandomButton()
     {
-        prefabs = new List<GameObject>();
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < spawnCount; i++)
         {
             var pos = new Vector3(Random.Range(0, 10), Random.Range(10, 30), Random.Range(0, 10));
             var newPrefab = Instantiate(prefab, pos, Quaternion.identity);
